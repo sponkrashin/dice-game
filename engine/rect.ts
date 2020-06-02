54c7621d1c3601279fbf9a9dd3f8044a6ee28050
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2020-06-02 19:17:10 +0300

    Added methods to Rect class to get rect value and rect intersection.

--- engine/rect.ts
+++ engine/rect.ts
@@ -3,7 +3,48 @@ import { Size } from './size';
 export class Rect {
   constructor(public readonly top: number, public readonly left: number, public readonly size: Size) {}
 
+  getValue(): number {
+    return this.size.width * this.size.height;
+  }
+
   getIntersection(rect: Rect): Rect {
-    throw new Error('Method not implemented');
+    const minLeft = Math.min(this.left, rect.left);
+    const minTop = Math.min(this.top, rect.top);
+    const maxRight = Math.max(this.left + this.size.width, rect.left + rect.size.width);
+    const maxBottom = Math.max(this.top + this.size.height, rect.top + rect.size.height);
+
+    let intersectLeft = -1;
+    let intersectRight = -1;
+    let intersectTop = -1;
+    let intersectBottom = -1;
+
+    for (let x = minLeft; x < maxRight; ++x) {
+      for (let y = minTop; y < maxBottom; ++y) {
+        if (this.isPointInside(x, y) && rect.isPointInside(x, y)) {
+          if (intersectLeft === -1) {
+            intersectLeft = x;
+            intersectRight = x;
+            intersectTop = y;
+            intersectBottom = y;
+          } else {
+            intersectRight = Math.max(intersectRight, x);
+            intersectBottom = Math.max(intersectBottom, y);
+          }
+        }
+      }
+    }
+
+    if (intersectLeft === -1) {
+      return null;
+    }
+
+    return new Rect(intersectLeft, intersectRight, {
+      width: intersectRight - intersectLeft + 1,
+      height: intersectBottom - intersectTop + 1,
+    });
+  }
+
+  private isPointInside(x: number, y: number): boolean {
+    return this.left <= x && this.left + this.size.width > x && this.top <= y && this.top + this.size.height > y;
   }
 }
