d92dd0d11d98f40c10f91a08bb0e990162e87889
Maxim Irinarkhov <iimaxxxii@gmail.com>
2020-06-05 12:37:40 +0400

    Returned options for Lint, changed order of import

--- dice-game-ui-angular/src/app/app.module.ts
+++ dice-game-ui-angular/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -10,6 +9,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartGameComponent } from './start-game/start-game.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
