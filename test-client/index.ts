d7ffca58203c835d2054e3fe00f9e4e322db4034
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2020-06-12 19:25:33 +0300

    Modified test client according to latest game engine changes.

--- test-client/index.ts
+++ test-client/index.ts
@@ -2,8 +2,7 @@ import { SimpleGameEngine, Rect, SimpleDice } from 'engine';
 
 const engine = new SimpleGameEngine(10, 10);
 
-const positionDice = new SimpleDice(10);
-const sizeDice = new SimpleDice(6);
+const positionDices = [new SimpleDice(10), new SimpleDice(10)];
 
 engine.registerOnStateChanged((engine) => {
   engine.state.forEach((row) => {
@@ -19,17 +18,27 @@ engine.registerOnGameFinished((engine) => console.log(engine.players));
 engine.startGame();
 
 for (let i = 0; i < 10; ++i) {
-  const position = positionDice.roll();
-  const size = sizeDice.roll();
+  if (engine.isFinished) {
+    break;
+  }
+
+  const position = positionDices.map((d) => d.roll());
+  const size = engine.rollDices();
 
   const newRect = new Rect(position[0] - 1, position[1] - 1, size[0], size[1]);
   console.log(newRect);
 
-  try {
+  const isIntersected = engine.rects.length !== 0 && engine.rects.some((r) => r.getIntersection(newRect));
+  const isTouched = engine.rects.length === 0 || engine.rects.some((r) => r.getIsTouched(newRect));
+
+  if (isIntersected || !isTouched) {
+    console.log('Cannot place such rect, skipping turn');
+    engine.skipTurn(null);
+  } else {
     engine.addRect(null, newRect);
-  } catch (err) {
-    console.log(err.message);
   }
 }
 
-engine.finishGame();
+if (!engine.isFinished) {
+  engine.finishGame();
+}
