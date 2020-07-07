dd1c0bd9745aa1fc2a3b5675d04374112ab123e3
Maxim Irinarkhov <iimaxxxii@gmail.com>
2020-07-07 10:16:16 +0400

    Changed statistics component to show new vers of stat

--- dice-game-ui-angular/src/app/statistics/statistics.component.ts
+++ dice-game-ui-angular/src/app/statistics/statistics.component.ts
@@ -2,17 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 import { GameStatistics, StatisticsService } from '../services/statistics-service';
 
+export class StatisticsDTO {
+  constructor(
+    public gameType: string,
+    public fieldSize: string,
+    public turnsCount: number,
+    public score: number,
+    public isWinner: boolean
+  ) {}
+}
+
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss'],
 })
 export class StatisticsComponent implements OnInit {
-  statistics: GameStatistics[];
+  statistics: StatisticsDTO[];
   displayedColumns: string[];
 
   constructor(private statisticsService: StatisticsService) {
-    this.displayedColumns = ['playerId', 'gameType', 'score'];
+    this.displayedColumns = ['gameType', 'fieldSize', 'turnsCount', 'score', 'isWinner'];
   }
 
   ngOnInit(): void {
@@ -20,8 +30,19 @@ export class StatisticsComponent implements OnInit {
   }
 
   private getAllStatistics() {
+    const curPlayer = 'local player';
     this.statistics = this.statisticsService
-      .getAllStatistics()
-      .sort((game1, game2) => (game1.creatingDate < game2.creatingDate ? 1 : -1));
+      .getPlayerStatistics(curPlayer)
+      .sort((game1, game2) => (game1.creatingDate < game2.creatingDate ? 1 : -1))
+      .map(
+        (s) =>
+          new StatisticsDTO(
+            s.gameType,
+            `${s.fieldSize.width} x ${s.fieldSize.height}`,
+            s.playersStaistics?.find((ps) => ps.playerId === curPlayer)?.score ?? 0,
+            s.playersStaistics?.find((ps) => ps.playerId === curPlayer)?.turnsCount ?? 0,
+            s.winnerPlayer === curPlayer
+          )
+      );
   }
 }
